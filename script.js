a0118d95c181b43f004bbed116139880743e4ba3
koayyiting <koayyiting@gmail.com>
2021-12-03 18:36:43 +0800

    first js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let newForm = document.createElement("newform");
-var form = document.getElementsByClassName("newform");
+function addCR(){
+    var copy = document.getElementById('js.p');
+    var newText = document.createTextNode("Copyright © 2021 Koay Yi Ting. All Rights Reserved.");
+    copy.appendChild(newText); 
+}
