6d76a68fb4048659ed30ea8f24c0b3f8cbf2234b
koayyiting <koayyiting@gmail.com>
2021-12-03 22:59:41 +0800

    final updates

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+/*
+var image = document.getElementById('imagesch');
+function PriSch() {
+    image.src = "Images/pri4.jpg";
+}
+function SecSch() {
+    image.src = "Images/sec4.jpeg";
+}
+*/
+
 function imageChange() {
     var image = document.getElementById('imagesch');
-    if (image.src == "images/pri4.jpg") {
-        image.src = "images/sec4.jpeg";
+    if (image.src == "Images/pri4.jpg") {
+        image.src = "Images/sec4.jpeg";
     } else {
-        image.src = "images/pri4.jpg";
+        image.src = "Images/pri4.jpg";
     }
 }
